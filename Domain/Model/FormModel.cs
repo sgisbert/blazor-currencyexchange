b73f69495db59f1cd701a19071b2017991c9dc14
Sergi Gisbert <sgisbert@users.noreply.github.com>
2021-02-07 19:54:34 +0100

    Add FormModel

---
+++ Domain/Model/FormModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Domain.Model
+{
+    public class FormModel
+    {
+        public DateTime CurrencyDate;
+        public String Currency;
+        public long Amount;
+    }
+}
