bb9918a20aaab8b5f7696aeaaaec0c63e54b1617
Sergi Gisbert <sgisbert@users.noreply.github.com>
2021-02-21 18:29:51 +0100

    Legacy code cleanup

--- src/Startup.cs
+++ src/Startup.cs
@@ -1,4 +1,3 @@
-using BlazorCurrencyExchange.Data;
 using Domain;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -23,7 +22,6 @@ namespace BlazorCurrencyExchange
         {
             services.AddRazorPages();
             services.AddServerSideBlazor();
-            services.AddSingleton<WeatherForecastService>();
 
             services.InitDomain();
         }
